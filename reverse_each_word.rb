7aa934894264ae9beb65b5edf426a77e70b0bd8b
Juliana Connors <juliana.g.connors@gmail.com>
2020-07-14 01:45:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,18 @@ def reverse_each_word(sentence1)
  end
 end
 
+def reverse_each_word(sentence1)
+  sentence3 = []
+  sentence1.split.collect do |word|
+   sentence3 << word.reverse
+ end
+ sentence3.join(" ")
+end
+
+
+
+def hamburger(toppings)
+  toppings.collect do |topping|
+    puts "I love #{topping} on my burgers!"
+  end
+end
